2aadd682d337b53c4736137aa29937380cc7550a
Toast <bored@toasted.me>
2025-02-18 07:16:54 +0400

    Add board table

---
+++ web-chess/src/router/index.ts
@@ -0,0 +1,35 @@
+import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/",
+    name: "home",
+    component: HomeView,
+  },
+  {
+    path: "/about",
+    name: "about",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+  },
+  {
+    path: "/board",
+    name: "board",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/ChessBoard.vue"),
+  },
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+export default router;
---
+++ web-chess/src/views/ChessBoard.vue
@@ -0,0 +1,18 @@
+<template>
+  <table class="chessboard">
+    <tbody>
+      <tr v-for="row in 8" :key="row">
+        <td
+          v-for="col in 8"
+          :class="(row + col) % 2 === 0 ? 'white' : 'black'"
+          :key="col"
+        >
+          s
+        </td>
+      </tr>
+    </tbody>
+  </table>
+  <span v-for="(value, key) in 10" :key="value - key"
+    >{{ value }} {{ key }}</span
+  >
+</template>
